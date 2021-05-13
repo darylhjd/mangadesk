a736aad4df47d1a2f21f25deb55ae1d2df0f9482
Daryl <aka.hjd@gmail.com>
2021-05-13 15:11:20 +0800

    Better error handling.

--- inputs.go
+++ inputs.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/darylhjd/mangodex"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"os"
 )
 
 // setUniversalInputCaptures : Set input handlers for the app.
+// List of input captures: Ctrl+L
 func setUniversalInputCaptures(pages *tview.Pages) {
 	// Enable mouse.
 	app.EnableMouse(true)
@@ -16,17 +18,36 @@ func setUniversalInputCaptures(pages *tview.Pages) {
 		switch event.Key() {
 		case tcell.KeyCtrlL: // Login/Logout
 			ctrlLInput(pages)
+		case tcell.KeyCtrlH:
+			ctrlHInput(pages)
 		}
 		return event
 	})
 }
 
-// setMainPageTableInputCaptures : Set input handlers for the main page table.
-func setMainPageTableInputCaptures(table *tview.Table, sRows *map[int]struct{}) {
+// setMangaPageHandlers : Set input handlers for the manga page.
+func setMangaPageHandlers(pages *tview.Pages, grid *tview.Grid) {
+	grid.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEsc:
+			pages.RemovePage(MangaPageID)
+		}
+		return event
+	})
+}
+
+// setMangaPageTableHandlers : Set input handlers for the manga page table.
+func setMangaPageTableHandlers(pages *tview.Pages, table *tview.Table,
+	selected *map[int]struct{}, mr *mangodex.MangaResponse, cl *mangodex.ChapterList) {
+	// When user presses enter to confirm selected
+	table.SetSelectedFunc(func(row, column int) {
+		confirmChapterDownloads(pages, table, selected, row, mr, cl)
+	})
+
 	table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
 		case tcell.KeyCtrlE:
-			ctrlEInput(table, sRows)
+			ctrlEInput(table, selected) // Check for updates for each manga.
 		}
 		return event
 	})
@@ -54,11 +75,15 @@ func ctrlLInput(pages *tview.Pages) {
 	// This will decide the function of the modal.
 	switch dex.IsLoggedIn() {
 	case true: // User wants to logout.
-		title = "Logout\n"
+		title = "Logout\nStored credentials will be deleted.\n\n"
 		buttonFn = func() {
 			// Attempt logout
 			if err := dex.Logout(); err != nil {
-				panic(err)
+				ShowModal(pages, LoginLogoutFailureModalID, "Error logging out!", []string{"OK"},
+					func(i int, label string) {
+						pages.RemovePage(LoginLogoutFailureModalID)
+					})
+				return
 			}
 			// Remove the credentials file
 			_ = os.Remove(credFile)
@@ -84,18 +109,23 @@ func ctrlLInput(pages *tview.Pages) {
 		})
 }
 
-// ctrlEInput() : Handler for Ctrl+E input.
-// This input enables user to select a main page table row without activating the select action.
-// This is done by using a int array to keep track of the selected row indexes.
+// ctrlEInput : Handler for Ctrl+E input.
+// This input enables user to select a chapter table row without activating the select action.
+// This is done by using a int map to keep track of the selected row indexes.
 func ctrlEInput(table *tview.Table, sRows *map[int]struct{}) {
 	// Get the current row (and col, but we do not need that)
 	row, _ := table.GetSelection()
-	// If the row already exists in the map, then we remove it!
-	if _, ok := (*sRows)[row]; ok {
-		markChapterUnselected(table, row, tcell.ColorBlack, tcell.ColorWhite)
+	if _, ok := (*sRows)[row]; ok { // If the row already exists in the map, then we remove it!
+		markChapterUnselected(table, row)
 		delete(*sRows, row)
 	} else { // Else, we add the row into the map.
-		markChapterSelected(table, row, tcell.ColorLightSkyBlue, tcell.ColorBlack)
+		markChapterSelected(table, row)
 		(*sRows)[row] = struct{}{}
 	}
+}
+
+// ctrlHInput : Handler for Ctrl+H input.
+// This shows the help page to the user.
+func ctrlHInput(pages *tview.Pages) {
+	ShowHelpPage(pages)
 }
--- pages.go
+++ pages.go
@@ -10,18 +10,24 @@ import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"net/url"
+	"strings"
 )
 
 const (
 	LoginPageID = "login_page"
 	MainPageID  = "main_page"
+	MangaPageID = "manga_page"
+	HelpPageID  = "help_page"
 
-	LoginFailureModalID     = "login_failure_modal"
-	LoginLogoutCfmModalID   = "logout_modal"
-	DownloadChaptersModalID = "download_chapters_modal"
+	LoginLogoutFailureModalID   = "login_failure_modal"
+	LoginLogoutCfmModalID       = "logout_modal"
+	StoreCredentialErrorModalID = "store_cred_error_modal"
+	DownloadChaptersModalID     = "download_chapters_modal"
+	DownloadErrorModalID        = "download_error_modal"
+	GenericAPIErrorModalID      = "api_error_modal"
 )
 
-// ShowLoginPage : Page to show login form.
+// ShowLoginPage : Show login page.
 func ShowLoginPage(pages *tview.Pages) {
 	// Create the form
 	form := tview.NewForm()
@@ -49,49 +55,16 @@ func ShowLoginPage(pages *tview.Pages) {
 	// Create a new grid for the form. This is to align the form to the centre.
 	grid := tview.NewGrid().SetColumns(0, 0, 0).SetRows(0, 0, 0)
 	grid.AddItem(form, 0, 0, 3, 3, 0, 0, true).
-		AddItem(form, 1, 1, 1, 1, 20, 50, true)
+		AddItem(form, 1, 1, 1, 1, 32, 70, true)
 
 	pages.AddPage(LoginPageID, grid, true, false)
 	app.SetFocus(grid)
 	pages.SwitchToPage(LoginPageID)
 }
 
-// ShowMainPage : Creates the basic template for the main page.
-// Works for both Guest and Logged account.
+// ShowMainPage : Show the main page. Can be for logged user or guest user.
 func ShowMainPage(pages *tview.Pages) {
-	// Whether to show guest or logged main page.
-	var (
-		chaps      *mangodex.ChapterList
-		mainTitle  string
-		tableTitle string
-		params     = url.Values{}
-	)
-	if dex.IsLoggedIn() {
-		u, err := dex.GetLoggedUser()
-		if err != nil {
-			panic(err)
-		}
-		mainTitle = fmt.Sprintf("Welcome to MangaDex, [green]%s!", u.Data.Attributes.Username)
-		tableTitle = "Feed"
-		params.Add("limit", "50")
-		params.Add("locales[]", "en")
-		chaps, err = dex.GetUserFollowedMangaChapterFeed(params)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		var err error
-		mainTitle = "Welcome to MangaDex, [red]Guest!"
-		tableTitle = "Recently Uploaded Chapters"
-		params.Add("limit", "50")
-		params.Add("order[createdAt]", "desc")
-		chaps, err = dex.ChapterList(params)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	// Create main page grid.
+	// Create the base main grid.
 	// 15x15 grid.
 	var g []int
 	for i := 0; i < 15; i++ { // This is to create 15 grids.
@@ -99,57 +72,275 @@ func ShowMainPage(pages *tview.Pages) {
 	}
 	grid := tview.NewGrid().SetColumns(g...).SetRows(g...)
 	// Set grid attributes.
-	grid.SetTitle(mainTitle).
-		SetBackgroundColor(tcell.ColorBlack).
-		SetTitleColor(tcell.ColorOrange).
+	grid.SetTitleColor(tcell.ColorOrange).
 		SetBorderColor(tcell.ColorLightGrey).
 		SetBorder(true)
 
-	// Use a table to show chapter information.
-	// len(chaps)x2 table.
+	// Create the base main table.
 	table := tview.NewTable()
-	// Set the table header
-	mangaHeader := tview.NewTableCell("MANGA").
-		SetAlign(tview.AlignCenter).
-		SetTextColor(tcell.ColorPapayaWhip).
-		SetSelectable(false)
-	chapterHeader := tview.NewTableCell("CHAPTER").
-		SetAlign(tview.AlignLeft).
-		SetTextColor(tcell.ColorPapayaWhip).
-		SetSelectable(false)
-	table.SetCell(0, 0, mangaHeader). // This sets the first row as fixed as it is used as the header.
-						SetCell(0, 1, chapterHeader).
-						SetFixed(1, 0)
-	// Set table attributes
-	table.SetSelectable(true, false).
-		SetSelectedStyle(tcell.Style{}.Background(tcell.ColorPeachPuff).Foreground(tcell.ColorBlack)).
-		SetSeparator('|').
-		SetBordersColor(tcell.ColorGrey).
-		SetTitle(tableTitle).
-		SetTitleColor(tcell.ColorLightSkyBlue).
-		SetBorder(true)
+	table.SetSelectable(true, false). // Sets only the rows to be selectable
+						SetSeparator('|').
+						SetBordersColor(tcell.ColorGrey).
+						SetTitleColor(tcell.ColorLightSkyBlue).
+						SetBorder(true)
 
-	// Set custom input handlers for the table.
-	var selected = map[int]struct{}{}
-	setMainPageTableInputCaptures(table, &selected)
-	table.SetSelectedFunc(func(row, col int) {
-		mainPageTableSelectedFunc(pages, table, row, &selected, chaps)
-	})
-
-	// Add the rows of data into the table.
-	for index, chapR := range chaps.Results {
-		titleCell := tview.NewTableCell(fmt.Sprintf("%-50s", chapR.Data.Attributes.Title)).
-			SetMaxWidth(50)
-		table.SetCell(index+1, 0, titleCell)
-		table.SetCellSimple(index+1, 1, chapR.Data.Attributes.Chapter)
-	}
-
-	// Add the list to the main grid.
+	// Add the table to the grid.
 	grid.AddItem(table, 0, 0, 15, 15, 0, 0, true)
+
+	if dex.IsLoggedIn() {
+		setUpLoggedMainPage(pages, grid, table)
+	} else {
+		setUpGuestMainPage(pages, grid, table)
+	}
 
 	pages.AddPage(MainPageID, grid, true, false)
 	app.SetFocus(grid)
 	pages.SwitchToPage(MainPageID)
+}
+
+// setUpLoggedMainPage : Set up the main page for a logged user.
+func setUpLoggedMainPage(pages *tview.Pages, grid *tview.Grid, table *tview.Table) {
+	// For logged users, we fill the table with their followed manga.
+	// Get user information
+	username := "?"
+	if u, err := dex.GetLoggedUser(); err == nil {
+		username = u.Data.Attributes.Username
+	}
+	grid.SetTitle(fmt.Sprintf("Welcome to MangaDex, [lightgreen]%s!", username))
+	table.SetTitle("Your followed manga")
+
+	titleColor := tcell.ColorLightGoldenrodYellow
+	statusColor := tcell.ColorSaddleBrown
+
+	// Set up table
+	mangaTitleHeader := tview.NewTableCell("Manga").
+		SetAlign(tview.AlignCenter).
+		SetTextColor(titleColor).
+		SetSelectable(false)
+	statusHeader := tview.NewTableCell("Status").
+		SetAlign(tview.AlignCenter).
+		SetTextColor(statusColor).
+		SetSelectable(false)
+	table.SetCell(0, 0, mangaTitleHeader).
+		SetCell(0, 1, statusHeader).
+		SetFixed(1, 0)
+
+	go func() {
+		// Get the manga list.
+		mangaList, err := dex.GetUserFollowedMangaList(50, 0)
+		if err != nil {
+			ShowModal(pages, GenericAPIErrorModalID, "Error getting followed manga.", []string{"OK"},
+				func(i int, label string) {
+					pages.RemovePage(GenericAPIErrorModalID)
+				})
+			return
+		}
+
+		// Set up the selected function.
+		table.SetSelectedFunc(func(row, column int) {
+			ShowMangaPage(pages, &(mangaList.Results[row-1]))
+		})
+
+		// Add the entries into the table.
+		for i, mr := range mangaList.Results {
+			// Create the manga title cell
+			mtCell := tview.NewTableCell(fmt.Sprintf("%-50s", mr.Data.Attributes.Title["en"])).
+				SetMaxWidth(50)
+			mtCell.Color = titleColor
+
+			// Create status cell
+			status := "-"
+			if mr.Data.Attributes.Status != nil {
+				status = strings.Title(*mr.Data.Attributes.Status)
+			}
+			sCell := tview.NewTableCell(fmt.Sprintf("%-15s", status)).
+				SetMaxWidth(15)
+			sCell.Color = statusColor
+
+			app.QueueUpdateDraw(
+				func() {
+					table.SetCell(i+1, 0, mtCell).
+						SetCell(i+1, 1, sCell)
+				})
+		}
+	}()
+}
+
+// setUpGuestMainPage : Set up the main page for a guest user.
+func setUpGuestMainPage(pages *tview.Pages, grid *tview.Grid, table *tview.Table) {
+	// For guest users, we fill the table with recently updated manga.
+	grid.SetTitle("Welcome to MangaDex, [red]Guest!")
+	table.SetTitle("Recently updated manga")
+
+	titleColor := tcell.ColorOrange
+	descColor := tcell.ColorLightGrey
+	tagColor := tcell.ColorLightSteelBlue
+
+	// Set up the table.
+	mangaTitleHeader := tview.NewTableCell("Manga").
+		SetAlign(tview.AlignCenter).
+		SetTextColor(titleColor).
+		SetSelectable(false)
+	descHeader := tview.NewTableCell("Description").
+		SetAlign(tview.AlignCenter).
+		SetTextColor(descColor).
+		SetSelectable(false)
+	tagHeader := tview.NewTableCell("Tags").
+		SetAlign(tview.AlignCenter).
+		SetTextColor(tagColor).
+		SetSelectable(false)
+	table.SetCell(0, 0, mangaTitleHeader).
+		SetCell(0, 1, descHeader).
+		SetCell(0, 2, tagHeader).
+		SetFixed(1, 0)
+
+	go func() {
+		// Get recently updated manga.
+		params := url.Values{}
+		params.Set("limit", "50")
+		mangaList, err := dex.MangaList(params)
+		if err != nil {
+			ShowModal(pages, GenericAPIErrorModalID, "Error loading manga list.", []string{"OK"},
+				func(i int, label string) {
+					pages.RemovePage(GenericAPIErrorModalID)
+				})
+			return
+		}
+
+		// Set up the selected function.
+		table.SetSelectedFunc(func(row, column int) {
+			ShowMangaPage(pages, &(mangaList.Results[row-1]))
+		})
+
+		// Add the entries into the table
+		for i, mr := range mangaList.Results {
+			// Create the manga title cell
+			mtCell := tview.NewTableCell(fmt.Sprintf("%-40s", mr.Data.Attributes.Title["en"])).
+				SetMaxWidth(40)
+			mtCell.Color = titleColor
+
+			// Create the description cell
+			desc := tview.Escape(fmt.Sprintf("%-70s", mr.Data.Attributes.Description["en"]))
+			descCell := tview.NewTableCell(desc).
+				SetMaxWidth(70)
+			descCell.Color = descColor
+
+			// Create the tag cell
+			tags := make([]string, len(mr.Data.Attributes.Tags))
+			for i, tag := range mr.Data.Attributes.Tags {
+				tags[i] = tag.Attributes.Name["en"]
+			}
+			tagCell := tview.NewTableCell(strings.Join(tags, ", "))
+			tagCell.Color = tagColor
+
+			app.QueueUpdateDraw(
+				func() {
+					table.SetCell(i+1, 0, mtCell).
+						SetCell(i+1, 1, descCell).
+						SetCell(i+1, 2, tagCell)
+				})
+		}
+	}()
+}
+
+// ShowMangaPage : Show the manga page.
+func ShowMangaPage(pages *tview.Pages, mr *mangodex.MangaResponse) {
+	// Create the base main grid.
+	// 15x15 grid.
+	var g []int
+	for i := 0; i < 15; i++ { // This is to create 15 grids.
+		g = append(g, -1)
+	}
+	grid := tview.NewGrid().SetColumns(g...).SetRows(g...)
+	// Set grid attributes
+	grid.SetTitleColor(tcell.ColorOrange).
+		SetBorderColor(tcell.ColorLightGrey).
+		SetTitle("Manga Information").
+		SetBorder(true)
+
+	// Set input handlers for this page
+	setMangaPageHandlers(pages, grid)
+
+	// Use a textview for basic information of the manga.
+	info := tview.NewTextView()
+	go func() {
+		setMangaInfo(info, mr)
+	}()
+	// Set textview attributes
+	info.SetWrap(true).SetWordWrap(true).
+		SetBorderColor(tcell.ColorLightGrey).
+		SetTitleColor(tcell.ColorLightSkyBlue).
+		SetTitle("About").
+		SetBorder(true)
+
+	// Use a table to show the chapters for the manga.
+	table := tview.NewTable()
+	// Set chapter headers
+	numHeader := tview.NewTableCell("Chap").
+		SetTextColor(tcell.ColorLightYellow).
+		SetSelectable(false)
+	titleHeader := tview.NewTableCell("Name").
+		SetTextColor(tcell.ColorLightSkyBlue).
+		SetSelectable(false)
+	table.SetCell(0, 0, numHeader).
+		SetCell(0, 1, titleHeader).
+		SetFixed(1, 0)
+	go func() {
+		setMangaChaptersTable(pages, table, mr)
+	}()
+	// Set table attributes
+	table.SetSelectable(true, false).
+		SetSeparator('|').
+		SetBordersColor(tcell.ColorGrey).
+		SetTitle("Read").
+		SetTitleColor(tcell.ColorLightSkyBlue).
+		SetBorder(true)
+
+	grid.AddItem(info, 0, 0, 5, 15, 0, 0, false).
+		AddItem(table, 5, 0, 10, 15, 0, 0, true).
+		AddItem(info, 0, 0, 15, 5, 0, 80, false).
+		AddItem(table, 0, 5, 15, 10, 0, 80, true)
+
+	pages.AddPage(MangaPageID, grid, true, false)
+	app.SetFocus(grid)
+	pages.SwitchToPage(MangaPageID)
+}
+
+// ShowHelpPage : Show the help page to the user.
+func ShowHelpPage(pages *tview.Pages) {
+	helpText := "Keyboard Mappings\n" +
+		"-----------------------------\n\n" +
+		"Universal\n" +
+		fmt.Sprintf("%-15s:%15s\n", "Ctrl + L", "Login/Logout") +
+		fmt.Sprintf("%-15s:%15s\n", "Ctrl + H", "Show Help") +
+		fmt.Sprintf("%-15s:%15s\n\n", "Ctrl + S", "Search") +
+		"Manga Page\n" +
+		fmt.Sprintf("%-15s:%15s\n", "Ctrl + E", "Select mult.") +
+		fmt.Sprintf("%-15s:%15s\n\n", "Enter", "Start download") +
+		"Works for most pages\n" +
+		fmt.Sprintf("%-15s:%15s\n", "Esc", "Go back")
+
+	help := tview.NewTextView()
+	help.SetText(helpText).
+		SetTextAlign(tview.AlignCenter).
+		SetBorderColor(tcell.ColorLightGrey).
+		SetBorder(true)
+
+	// Create a new grid for the text view so we can align it to the center.
+	grid := tview.NewGrid().SetColumns(0, 0, 0, 0).SetRows(0, 0, 0, 0).
+		AddItem(help, 0, 0, 4, 4, 0, 0, false).
+		AddItem(help, 1, 1, 2, 2, 30, 100, false)
+	grid.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEsc:
+			pages.RemovePage(HelpPageID)
+		}
+		return event
+	})
+
+	pages.AddPage(HelpPageID, grid, true, false)
+	app.SetFocus(grid)
+	pages.SwitchToPage(HelpPageID)
 }
 
 // ShowModal : Convenience function to create a modal.
