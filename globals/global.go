04130ba46228b08bde464a0dfe82e3e61a9f16fb
Daryl <aka.hjd@gmail.com>
2021-05-14 22:22:13 +0800

    Tidy up.

--- globals/global.go
+++ globals/global.go
@@ -11,22 +11,16 @@ var (
 )
 
 const (
-	LoginPageID  = "login_page"
+	LoginPageID  = "login_page" // Main Pages and their IDs
 	MainPageID   = "main_page"
 	MangaPageID  = "manga_page"
 	HelpPageID   = "help_page"
 	SearchPageID = "search_page"
 
-	LoginLogoutFailureModalID   = "login_failure_modal"
+	LoginLogoutFailureModalID   = "login_failure_modal" // Modals and their IDs
 	LoginLogoutCfmModalID       = "logout_modal"
 	StoreCredentialErrorModalID = "store_cred_error_modal"
 	DownloadChaptersModalID     = "download_chapters_modal"
 	DownloadFinishedModalID     = "download_error_modal"
 	GenericAPIErrorModalID      = "api_error_modal"
-)
-
-const (
-	UsrDir       = "usr"
-	CredFileName = "cred"
-	DownloadDir  = "downloads"
 )
