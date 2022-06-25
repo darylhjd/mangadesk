20e3d4206bae4fac79d1f85c8a7d78346fee3a6d
darylhjd <53652695+darylhjd@users.noreply.github.com>
2022-06-25 12:05:47 +0800

    feat: upgrade minimum go version in go mod. (#75)

--- app/core/app.go
+++ app/core/app.go
@@ -12,7 +12,7 @@ import (
 // App : Global App variable.
 var (
 	App        *MangaDesk
-	AppVersion = "MangaDesk v0.7.7"
+	AppVersion = "MangaDesk v0.7.8"
 )
 
 // MangaDesk : The client for this application.
--- vendor/github.com/darylhjd/mangodex/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/darylhjd/mangodex
-
-go 1.16
